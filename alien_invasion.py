17196d5c2b5cb3ec26cdda23ccebdd2e350a1594
20390680 <20390680>
2022-06-25 14:56:09 +0300

    Added handling of keystrokes, travel restrictions, refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,22 +10,60 @@ class AlienInvasion:
     def __init__(self):
     #Создание игрового окна
         pygame.init()
-        self.setting = Settings()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((self.setting.screen_width, self.setting.screen_height))
+        # Оконный режим
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+
+        # Полноэкранный режим
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+
         pygame.display.set_caption('Kill Aliens')
         self.ship = Ship(self)
 
     def run(self):
     #Запуск оснвого цикла
         while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self.check_events()
+            self.update_screen()
+            self.ship.update()
 
-            self.screen.fill(self.setting.bg_color)
-            self.ship.blitme()
-            pygame.display.flip()
+
+    def check_events(self):
+        # обработка нажатия клавиш и мышки
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            # проверка нажатие клавиши и перемещение корабля
+            elif event.type == pygame.KEYDOWN:
+                self.check_keydown_enevts(event)
+            # если отпустить <- или -> то корабль перестанет двигаться
+            elif event.type == pygame.KEYUP:
+                self.check_keyup_enevets(event)
+
+    def check_keydown_enevts(self, event):
+        # логоика нажатия клавиш
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def check_keyup_enevets(self, event):
+        # логика если отпустить клавишу
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def update_screen(self):
+        # Обновляет изображения на экране
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- ship.py
+++ ship.py
@@ -5,6 +5,8 @@ class Ship():
         # Иницилизирует корабль и задает его наччальную позицию
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
 
         #загружает изображения корабля и получает прямоугольник
         self.image = pygame.image.load('images/ship1.bmp')
@@ -12,6 +14,25 @@ class Ship():
 
         #каждый новый корабль появляеться у нижнего края экрана
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #Сохранение вещественной коориданты
+        self.x = float(self.rect.x)
+
+        # флаг для не прервного перемещения
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        # обновляет позицию корабля с учетом флага
+        # обновлять будем атрибут x
+
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # обновляем атрибут rect
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
